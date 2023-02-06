4926ecd20684da1ae36f9529c52af681598e6c83
TR559 <uzy5@fresnou.org>
2023-02-06 13:43:30 -0800

    initial commit

---
+++ chaperonereview.js
@@ -0,0 +1,7 @@
+console.log(17+2);
+console.log(17%2);
+console.log(17+2=${17+2});
+console.log(25>=20);
+console.log(25.100&&25>10);
+console.log(25>100||25>10);
+console.log(3*2>1||25+71!=17%5);
---
+++ datatypes.js
@@ -0,0 +1,37 @@
+console.log(4);
+
+//This is a comment
+
+//Numbers
+// Integers
+console.log(-3);
+console.log(8);
+console.log(7);
+
+//Floating Point Numbers (Floats)
+console.log(3.14);
+console.log(-0.123456);
+
+//Strings
+//Characters,letters symbols,spaces
+console.log("Hello World");
+console.log('!@#$ is also a String');
+console.log(`backtic by the 1`);
+
+//Aithmetic Operators (math)
+console.log(14+2);
+console.log(14-2);
+console.log(14*2); //shift-8 gives *
+console.log(14/2);
+console.log(14%2); //% is called MOD
+                   //it means remainder after dividing
+
+//Concatenation means combine strings
+console.log("My name "+" is franklin ");
+console.log('my '+' '+'age'+' '+'is'+14+'.');
+console.log("your age is\n16.");
+
+//template literal - plug in some calculated value
+//Use backticks `` by the 1 on your keyboard
+console.log(`15 divided by 10 = ${15/10} asdfsadf`);
+console.log(17+2);
